3b6970fc84dae4efebc3c787b22e809bc67ee727
Diana Chepkoech <dianachepkoech336@gmail.com>
2023-11-30 21:27:13 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
-def oxford_comma(items):
-    return None
+
+
+def oxford_comma(elements):
+    if len(elements) == 0:
+        return ""
+    elif len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        # Join all elements except the last one with commas
+        joined_elements = ", ".join(elements[:-1])
+        # Add Oxford comma and the last element
+        result = f"{joined_elements}, and {elements[-1]}"
+        return result
